057d1a82b4ee879b2610661b3ddc6d725b6674d6
duyd <duyd@segimail.com>
2024-04-01 10:43:34 +0800

    juc

---
+++ demo-juc/src/main/java/com/dyd/jucs/Test.java
@@ -0,0 +1,15 @@
+package com.dyd.jucs;
+
+/**
+ * @author ：duyd@segimail.com
+ * @class ：com.dyd.jucs.Test
+ * @date ：Created in 2024/4/1 10:40
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
